e68b40b6c04bb210647c9b4e676bcdfe1184dbfa
ra1nrouge <slumreaper@Slumreaper.local>
2017-02-04 00:52:22 -0500

    Basic sign in layout complete

--- Dataservice.swift
+++ Dataservice.swift
@@ -55,4 +55,3 @@ class DataService {
   }
   
 }
-K
--- SignInVC.swift
+++ SignInVC.swift
@@ -87,7 +87,7 @@ class SignInVC: UIViewController {
   
   func completeSignIn(id: String, userData: Dictionary<String, String>) {
     DataService.ds.createFirbaseDBUser(uid: id, userData: userData)
-    //let keychainResult = KeychainWrapper.setString(id, forKey: KEY_UID)
+    // let keychainResult = KeychainWrapper.setString(id, forKey: KEY_UID)
     let keychainResult = KeychainWrapper.defaultKeychainWrapper.set(id, forKey: KEY_UID)
     print("User: Data saved to keychain \(keychainResult)")
     performSegue(withIdentifier: "goToFeed", sender: nil)
